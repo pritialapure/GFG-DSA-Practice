e693816fca055b680cfc60d57e06685ffb666d3d
Priti <pritialapure@gmail.com>
2026-06-25 09:12:42 +0530

    Updated

--- Arrays/two-pointers-approach/ValidPalindrome.cpp
+++ Arrays/two-pointers-approach/ValidPalindrome.cpp
@@ -1,3 +1,10 @@
+
+
+// Problem: Palindrome String
+// GFG: https://www.geeksforgeeks.org/problems/palindrome-string0817/1
+
+
+
 class Solution {
   public:
     bool isPalindrome(string& s) {
@@ -14,18 +21,3 @@ class Solution {
     }
 };
 
-// Given a string s, return true if the string is a palindrome. Otherwise, return false.
-
-//A string is considered a palindrome if it reads the same forwards and backwards.
-
-//Examples :
-
-//Input: s = "abba"
-//Output: true
-//Explanation: "abba" reads the same forwards and backwards, so it is a palindrome.
-//Input: s = "abc" 
-//Output: false
-//Explanation: "abc" does not read the same forwards and backwards, so it is not a palindrome.
-//Constraints:
-//1 ≤ s.size() ≤ 106
-//The string s contains only lowercase english letters (a-z).
